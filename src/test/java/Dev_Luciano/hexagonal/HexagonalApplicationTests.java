ea5ef1fa5cbf3a6cbdb615ff3b08060be2fd8299
LucianoSant006 <santiagoluciano383@gmail.com>
2025-01-25 17:07:49 -0300

    start project

---
+++ src/test/java/Dev_Luciano/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package Dev_Luciano.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
